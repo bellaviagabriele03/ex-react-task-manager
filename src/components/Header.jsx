4d08c6665a9b1047732d42fd6e79e6ec34b76531
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2026-05-13 15:41:32 +0200

    FEAT: scafolding, repo back-end clonata, Header di base definito

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom"
+
+
+export default function Header() {
+
+    const links = [
+        { name: "TASK", path: "/" },
+        { name: "ADD TASK", path: "/addtask" },
+    ]
+
+    
+
+    return (
+        <header>
+            <div>
+                {links.map((l) => (
+                    <NavLink to={l.path}>{l.name}</NavLink>
+                ))}
+            </div>
+        </header>
+    )
+}
